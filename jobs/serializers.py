818e58818ebb50268a7903dd69b83ab22cebcad7
Mohamad anas fattoum <samaanas30@gmail.com>
2024-05-18 09:32:01 +0200

    add show Category, Company name with StringRelatedField in jobsdetailserializers

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
-from .models import Jobs , Company, Category
+from .models import Jobs , Company, Category, User
 
 class JobsListSerializer(serializers.ModelSerializer):
     company= serializers.StringRelatedField()
     category= serializers.StringRelatedField()
+    user= serializers.StringRelatedField()
     class Meta:
         model =  Jobs
         fields = '__all__'
 
 
 class JobsDetailSerializer(serializers.ModelSerializer):
-    
+    company= serializers.StringRelatedField()
+    category= serializers.StringRelatedField()    
+    user= serializers.StringRelatedField()    
+
     class Meta:
         model =  Jobs
         fields = '__all__'
@@ -28,3 +32,4 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model =  Category
         fields = '__all__'
+
