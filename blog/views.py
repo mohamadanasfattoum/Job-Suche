eb6cd8136475701c239317706fa59f1834212a8d
Mohamad anas fattoum <samaanas30@gmail.com>
2024-05-18 10:35:50 +0200

    created post generic views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Comments
 
-# Create your views here.
+
+
+class PostList(generic.ListView):
+    model=Post
+
+
+class PostDetail(generic.DetailView):
+    model=Post
+
+class PostDetail(generic.CreateView):
+    model=Post
+
+
+class PostDetail(generic.DeleteView):
+    model=Post
+
+
+class PostDetail(generic.UpdateView):
+    model=Post
